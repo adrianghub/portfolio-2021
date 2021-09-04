252d624a12ed17258802c285eae438a15ac86fab
Adrian Zinko <adrian.zinko@ardentcode.com>
2021-09-05 00:23:30 +0200

    clean up default vite starter boilerplate

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <>
+    <h1>Home Page</h1>
+      {/* Home */}
+      {/* About */}
+      {/* Skills */}
+      {/* Projects */}
+      {/* Contact */}
+    </>
+  );
+};
+
+export default HomePage;
