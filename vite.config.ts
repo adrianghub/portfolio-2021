ff545d1e6a3db3a8671ed5969db5d3e2ac4652ff
Adrian Zinko <adrian.zinko@ardentcode.com>
2021-09-28 14:03:31 +0200

    feat: add pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  plugins: [
+    reactRefresh(),
+    VitePWA({
+      includeAssets: ["robots.txt"],
+      manifest: {
+        name: "Dev Portfolio | Adrian Zinko",
+        short_name: "az-portfolio",
+        description:
+          "Dev Portfolio | Adrian Zinko - Personal developer portfolio website.",
+        theme_color: "#ffffff",
+      },
+    }),
+  ],
+});
